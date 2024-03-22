8c444394dd0aaf834354ff86084c5a4b8e9f4cd2
raseltpi30 <rasel52530@gmail.com>
2024-03-22 10:47:38 +0600

    Startup

---
+++ routes/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('admin-login', [App\Http\Controllers\Auth\LoginController::class, 'adminLogin'])->name('admin.login');
+
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')->middleware('is_admin');
