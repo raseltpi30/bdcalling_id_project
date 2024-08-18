a63f2691fee72df6d352658caa03dfae9f952981
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-18 10:40:01 +0600

    install

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -34,7 +34,7 @@
 
                         <div class="info-box-content">
                             <span class="info-box-text">Inventory</span>
-                            <span class="info-box-number">5,200</span>
+                            <span class="info-box-number">5,100</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
